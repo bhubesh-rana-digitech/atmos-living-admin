c462035a01f5fd8beeb01abff9effcf5c12d9270
manisha.dhimaan <manisha.dhimaan@digitechsoft.com>
2024-08-15 16:44:01 +0530

    inner pages

--- src/components/manage-amenity/AmenityUploadingInput.tsx
+++ src/components/manage-amenity/AmenityUploadingInput.tsx
@@ -14,17 +14,17 @@ function AmenityUploadingInput({ amenityData }) {
             <div className="icon-group">
                 <label className="form-label mb-2 inline-block">{item.uploadHeading}</label>
                 <div className="create-amenity-now">
-                    <div className="input-group flex flex-wrap items-stretch w-full relative">
-                        <div className="upload-icon-now w-[50px] bg-white relative border-2 border-[#dee2e6] rounded-tl-[8px] rounded-bl-[8px]">
-                            <label className="flex items-center justify-center h-full w-full" htmlFor={`UploadAmen${index}`}>
-                                <input type="file" id={`UploadAmen${index}`} className="absolute left-[-9999px] top-0 " />
-                                <div className="upload-icon flex justify-center items-center w-[50px] h-[50px] bg-[rgba(0,0,0,0.08)]">
-                                    <Image src={item.uploadImg} alt="Upload" title="" className="w-4 h-4" />
-                                </div>
-                            </label>
+                        <div className="input-group flex flex-wrap items-stretch w-full relative">
+                            <div className="upload-icon-now w-[50px] bg-white relative border-2 border-[#dee2e6] rounded-tl-[8px] rounded-bl-[8px]">
+                                <label className="flex items-center justify-center h-full w-full" htmlFor={`UploadAmen${index}`}>
+                                    <input type="file" id={`UploadAmen${index}`} className="absolute left-[-9999px] top-0 " />
+                                    <div className="upload-icon flex justify-center items-center w-[50px] h-[50px] bg-[rgba(0,0,0,0.08)]">
+                                        <Image src={item.uploadImg} alt="Upload" title="" className="w-4 h-4" />
+                                    </div>
+                                </label>
+                            </div>
+                            <input type="text" placeholder={item.enterPlaceholderText} className="form-control rounded-1 h-auto shadow-none rounded-tr-[8px] rounded-br-[8px] p-3" />
                         </div>
-                        <input type="text" placeholder={item.enterPlaceholderText} className="form-control rounded-1 h-auto shadow-none rounded-tr-[8px] rounded-br-[8px] p-3" />
-                    </div>
                 </div>
             </div>
         </li>
